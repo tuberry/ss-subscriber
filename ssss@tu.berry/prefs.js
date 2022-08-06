08f35bacaa73a36896c9f50c4f8833f0fdc8055c
tuberry <17917040+tuberry@users.noreply.github.com>
2022-08-06 19:24:33 +0800

    cleanup
    cleanup: fields in prefs.js
    typo: fix typo in ui.js

--- ssss@tu.berry/prefs.js
+++ ssss@tu.berry/prefs.js
@@ -31,47 +31,37 @@ class SSSubscriberPrefs extends Adw.PreferencesGroup {
     constructor() {
         super();
         this._buildWidgets();
-        this._bindValues();
         this._buildUI();
     }
 
     _buildWidgets() {
-        this._field_local_port = new UI.Spin(0, 65535, 1);
-        this._field_local_time = new UI.Spin(0, 1000, 50);
-        this._field_filename   = new UI.File({ filter: 'application/json' });
-        this._restart_btn      = new Gtk.Button({ label: _('Resart'), valign: Gtk.Align.CENTER });
-        this._field_additional = new UI.LazyEntry('{ "fast_open": true }', _('JSON format, can be blank'));
-        this._field_subs_link  = new UI.LazyEntry('https://www.example.com', _('Subscription link (SSD only)'));
-        this._field_restart    = new UI.LazyEntry('systemctl --user restart shadowsocks@ssss.service', _('Command to restart'));
-        this._field_local_addr = new Gtk.Entry({ placeholder_text: 'local_address', tooltip_text: _('Can be blank'), valign: Gtk.Align.CENTER });
+        this._restart_btn = new Gtk.Button({ label: _('Resart'), valign: Gtk.Align.CENTER });
+        this._field = {
+            PORT:     ['value', new UI.Spin(0, 65535, 1)],
+            TIME:     ['value', new UI.Spin(0, 1000, 50)],
+            FILE:     ['file',  new UI.File({ filter: 'application/json' })],
+            ADDITION: ['text',  new UI.LazyEntry('{ "fast_open": true }', _('JSON format, can be blank'))],
+            LINK:     ['text',  new UI.LazyEntry('https://www.example.com', _('Subscription link (SSD only)'))],
+            RESTART:  ['text',  new UI.LazyEntry('systemctl --user restart shadowsocks@ssss.service', _('Command to restart'))],
+            ADDR:     ['text',  new Gtk.Entry({ placeholder_text: 'local_address', tooltip_text: _('Can be blank'), valign: Gtk.Align.CENTER })],
+        };
+        Object.entries(this._field).forEach(([x, [y, z]]) => gsettings.bind(Fields[x], z, y, Gio.SettingsBindFlags.DEFAULT));
+        this._restart_btn.connect('clicked', this._updateConfig.bind(this));
     }
 
     _buildUI() {
         [
-            [[_('Subs link')], this._field_subs_link],
-            [[_('Conf file')], this._field_filename],
-            [[_('Timeout')], this._field_local_time],
-            [[_('Addr &amp; port')], this._field_local_addr, this._field_local_port],
-            [[_('Addtional')], this._field_additional],
+            [[_('Subs link')],       this._field.LINK[1]],
+            [[_('Conf file')],       this._field.FILE[1]],
+            [[_('Timeout')],         this._field.TIME[1]],
+            [[_('Addr &amp; port')], this._field.ADDR[1], this._field.PORT[1]],
+            [[_('Addtional')],       this._field.ADDITION[1]],
         ].forEach(xs => this.add(new UI.PrefRow(...xs)));
         let row = new Adw.ActionRow();
         row.add_prefix(this._restart_btn);
-        row.add_suffix(this._field_restart);
-        row.set_activatable_widget(this._field_restart);
+        row.add_suffix(this._field.RESTART[1]);
+        row.set_activatable_widget(this._field.RESTART[1]);
         this.add(row);
-    }
-
-    _bindValues() {
-        [
-            [Fields.PORT,     this._field_local_port, 'value'],
-            [Fields.TIME,     this._field_local_time, 'value'],
-            [Fields.FILE,     this._field_filename,   'file'],
-            [Fields.ADDR,     this._field_local_addr, 'text'],
-            [Fields.ADDITION, this._field_additional, 'text'],
-            [Fields.LINK,     this._field_subs_link,  'text'],
-            [Fields.RESTART,  this._field_restart,    'text'],
-        ].forEach(xs => gsettings.bind(...xs, Gio.SettingsBindFlags.DEFAULT));
-        this._restart_btn.connect('clicked', this._updateConfig.bind(this));
     }
 
     async _updateConfig() {
@@ -96,4 +86,3 @@ class SSSubscriberPrefs extends Adw.PreferencesGroup {
         };
     }
 }
-
